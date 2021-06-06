25e0f511748cc36afbe812b46a9d4ac5cc4197ec
Naru-hub <1208ny0324@gmail.com>
2021-06-06 14:05:35 +0000

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users,only:[:index,:show,:edit,:update]
-  resources :books,only:[:index,:show,:create,:edit,:update,:destroy]
+  resources :users, only:[:index, :show, :edit, :update]
+  resources :books, only:[:index, :show, :create, :edit, :update, :destroy] do
+    resources :book_comments, only:[:create, :destroy]
+
+  end
 end
